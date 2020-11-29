ffa9180f95625312efe3059608a1ac60235eb56f
yqxd <xuz19556@gmail.com>
2020-11-29 08:38:56 +0800

    Init commit

--- mid-ab-prunning.py
+++ mid-ab-prunning.py
@@ -1,6 +1,10 @@
+# -------------命令测试
+# piskvork.exe -p xxx.exe FIVEROW.zip -opening 1 -rule 0 -memory 512 -timeturn 15000 -timematch 90
+# -------------编译指令
+# pyinstaller example.py pisqpipe.py --name pbrain-pyrandom.exe --onefile
+
 import random
 import pisqpipe as pp
-import win32gui
 from pisqpipe import DEBUG_EVAL, DEBUG
 
 pp.infotext = 'name="pbrain-pyrandom", author="Jan Stransky", version="1.0", country="Czech Republic", www="https://github.com/stranskyjan/pbrain-pyrandom"'
@@ -8,181 +12,113 @@ pp.infotext = 'name="pbrain-pyrandom", author="Jan Stransky", version="1.0", cou
 MAX_BOARD = 100
 board = [[0 for i in range(MAX_BOARD)] for j in range(MAX_BOARD)]
 
+########################## self defined function ##############################################################
+values = [[0 for i in range(MAX_BOARD)] for j in range(MAX_BOARD)]  # rate for all values
+dvalue = [[[0 for k in range(8)] for i in range(MAX_BOARD)] for j in range(MAX_BOARD)]
 
-class game:
-    MAX_BOARD = MAX_BOARD
-    board = board
-    nowx = 0
-    nowy = 0
 
-    def brain_init(self):
-        if pp.width < 5 or pp.height < 5:
-            pp.pipeOut("ERROR size of the board")
-            return
-        if pp.width > self.MAX_BOARD or pp.height > self.MAX_BOARD:
-            pp.pipeOut("ERROR Maximal board size is {}".format(self.MAX_BOARD))
-            return
-        pp.pipeOut("OK")
+def update(x, y, dx, dy, col):
+    value = []
 
-    def brain_restart(self):
-        for x in range(pp.width):
-            for y in range(pp.height):
-                self.board[x][y] = 0
-        pp.pipeOut("OK")
 
-    def isFree(self, x, y):
-        return x >= 0 and y >= 0 and x < pp.width and y < pp.height and self.board[x][y] == 0
+def updateHelper(value, col, opcol):
+    if match(value, [])
 
-    def brain_my(self, x, y):
-        if self.isFree(x, y):
-            self.board[x][y] = 1
-        else:
-            pp.pipeOut("ERROR my move [{},{}]".format(x, y))
 
-    def brain_opponents(self, x, y):
-        if self.isFree(x, y):
-            self.board[x][y] = 2
-        else:
-            pp.pipeOut("ERROR opponents's move [{},{}]".format(x, y))
+def match(l1, l2):
+    return l1[0:5] == l2 or l1[1:6] == l2 or l1[2:7] == l2 or l1[3:8] == l2 or l1[4:9] == l2
 
-    def brain_block(self, x, y):
-        if self.isFree(x, y):
-            self.board[x][y] = 3
-        else:
-            pp.pipeOut("ERROR winning move [{},{}]".format(x, y))
 
-    def brain_takeback(self, x, y):
-        if x >= 0 and y >= 0 and x < pp.width and y < pp.height and self.board[x][y] != 0:
-            self.board[x][y] = 0
-            return 0
-        return 2
+########################### changed function ####################################################################
 
-    def brain_turn(self):
+
+########################### unchanged function ##################################################################
+def brain_init():
+    if pp.width < 5 or pp.height < 5:
+        pp.pipeOut("ERROR size of the board")
+        return
+    if pp.width > MAX_BOARD or pp.height > MAX_BOARD:
+        pp.pipeOut("ERROR Maximal board size is {}".format(MAX_BOARD))
+        return
+    pp.pipeOut("OK")
+
+
+def brain_restart():
+    for x in range(pp.width):
+        for y in range(pp.height):
+            board[x][y] = 0
+    pp.pipeOut("OK")
+
+
+def isFree(x, y):
+    return x >= 0 and y >= 0 and x < pp.width and y < pp.height and board[x][y] == 0
+
+
+def brain_my(x, y):
+    if isFree(x, y):
+        board[x][y] = 1
+    else:
+        pp.pipeOut("ERROR my move [{},{}]".format(x, y))
+
+
+def brain_opponents(x, y):
+    if isFree(x, y):
+        board[x][y] = 2
+    else:
+        pp.pipeOut("ERROR opponents's move [{},{}]".format(x, y))
+
+
+def brain_block(x, y):
+    if isFree(x, y):
+        board[x][y] = 3
+    else:
+        pp.pipeOut("ERROR winning move [{},{}]".format(x, y))
+
+
+def brain_takeback(x, y):
+    if x >= 0 and y >= 0 and x < pp.width and y < pp.height and board[x][y] != 0:
+        board[x][y] = 0
+        return 0
+    return 2
+
+
+def brain_turn():
+    if pp.terminateAI:
+        return
+    i = 0
+    while True:
+        x = random.randint(0, pp.width)
+        y = random.randint(0, pp.height)
+        i += 1
         if pp.terminateAI:
             return
-        i = 0
-        while True:
-            x = self.nowx
-            y = self.nowy
-            self.nowx += 1
-            self.nowy += 1
-            i += 1
-            if pp.terminateAI:
-                return
-            if self.isFree(x, y):
-                break
-        if i > 1:
-            pp.pipeOut("DEBUG {} coordinates didn't hit an empty field".format(i))
-        pp.do_mymove(x, y)
+        if isFree(x, y):
+            break
+    if i > 1:
+        pp.pipeOut("DEBUG {} coordinates didn't hit an empty field".format(i))
+    pp.do_mymove(x, y)
 
-    def brain_end(self):
-        pass
 
-    def brain_about(self):
-        pp.pipeOut(pp.infotext)
+def brain_end():
+    pass
 
-    def brain_eval(self, x, y):
+
+def brain_about():
+    pp.pipeOut(pp.infotext)
+
+
+if DEBUG_EVAL:
+    import win32gui
+
+
+    def brain_eval(x, y):
         # TODO check if it works as expected
         wnd = win32gui.GetForegroundWindow()
         dc = win32gui.GetDC(wnd)
         rc = win32gui.GetClientRect(wnd)
-        c = str(self.board[x][y])
+        c = str(board[x][y])
         win32gui.ExtTextOut(dc, rc[2] - 15, 3, 0, None, c, ())
         win32gui.ReleaseDC(wnd, dc)
-
-
-#
-# MAX_BOARD = 100
-# board = [[0 for i in range(MAX_BOARD)] for j in range(MAX_BOARD)]
-#
-#
-# def brain_init():
-#     if pp.width < 5 or pp.height < 5:
-#         pp.pipeOut("ERROR size of the board")
-#         return
-#     if pp.width > MAX_BOARD or pp.height > MAX_BOARD:
-#         pp.pipeOut("ERROR Maximal board size is {}".format(MAX_BOARD))
-#         return
-#     pp.pipeOut("OK")
-#
-#
-# def brain_restart():
-#     for x in range(pp.width):
-#         for y in range(pp.height):
-#             board[x][y] = 0
-#     pp.pipeOut("OK")
-#
-#
-# def isFree(x, y):
-#     return x >= 0 and y >= 0 and x < pp.width and y < pp.height and board[x][y] == 0
-#
-#
-# def brain_my(x, y):
-#     if isFree(x, y):
-#         board[x][y] = 1
-#     else:
-#         pp.pipeOut("ERROR my move [{},{}]".format(x, y))
-#
-#
-# def brain_opponents(x, y):
-#     if isFree(x, y):
-#         board[x][y] = 2
-#     else:
-#         pp.pipeOut("ERROR opponents's move [{},{}]".format(x, y))
-#
-#
-# def brain_block(x, y):
-#     if isFree(x, y):
-#         board[x][y] = 3
-#     else:
-#         pp.pipeOut("ERROR winning move [{},{}]".format(x, y))
-#
-#
-# def brain_takeback(x, y):
-#     if x >= 0 and y >= 0 and x < pp.width and y < pp.height and board[x][y] != 0:
-#         board[x][y] = 0
-#         return 0
-#     return 2
-#
-#
-#
-# def brain_turn():
-#     if pp.terminateAI:
-#         return
-#     i = 0
-#     while True:
-#         x = random.randint(0, pp.width)
-#         y = random.randint(0, pp.height)
-#         i += 1
-#         if pp.terminateAI:
-#             return
-#         if isFree(x, y):
-#             break
-#     if i > 1:
-#         pp.pipeOut("DEBUG {} coordinates didn't hit an empty field".format(i))
-#     pp.do_mymove(x, y)
-#
-#
-# def brain_end():
-#     pass
-#
-#
-# def brain_about():
-#     pp.pipeOut(pp.infotext)
-#
-#
-# if DEBUG_EVAL:
-#     import win32gui
-#
-#
-#     def brain_eval(x, y):
-#         # TODO check if it works as expected
-#         wnd = win32gui.GetForegroundWindow()
-#         dc = win32gui.GetDC(wnd)
-#         rc = win32gui.GetClientRect(wnd)
-#         c = str(board[x][y])
-#         win32gui.ExtTextOut(dc, rc[2] - 15, 3, 0, None, c, ())
-#         win32gui.ReleaseDC(wnd, dc)
 
 ######################################################################
 # A possible way how to debug brains.
@@ -222,19 +158,19 @@ def brain_turn():
 		logTraceBack()
 """
 ######################################################################
-A = game()
+
 # "overwrites" functions in pisqpipe module
-pp.brain_init = A.brain_init
-pp.brain_restart = A.brain_restart
-pp.brain_my = A.brain_my
-pp.brain_opponents = A.brain_opponents
-pp.brain_block = A.brain_block
-pp.brain_takeback = A.brain_takeback
-pp.brain_turn = A.brain_turn
-pp.brain_end = A.brain_end
-pp.brain_about = A.brain_about
+pp.brain_init = brain_init
+pp.brain_restart = brain_restart
+pp.brain_my = brain_my
+pp.brain_opponents = brain_opponents
+pp.brain_block = brain_block
+pp.brain_takeback = brain_takeback
+pp.brain_turn = brain_turn
+pp.brain_end = brain_end
+pp.brain_about = brain_about
 if DEBUG_EVAL:
-    pp.brain_eval = A.brain_eval
+    pp.brain_eval = brain_eval
 
 
 def main():
